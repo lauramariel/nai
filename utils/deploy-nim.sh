ca77d2840aa2f873695cd9ee675cb27a16293c26
Laura Jordana <lauram93@gmail.com>
2024-10-29 23:55:16 -0700

    test scripts for NIM/NGC

---
+++ utils/deploy-nim.sh
@@ -0,0 +1,62 @@
+# Create test pod for NIM
+
+STORAGE_CLASS="nai-nfs-storage"
+NGC_CLI_API_KEY=""
+
+# Create secret with NGC API key
+kubectl create secret generic ngc-secret \
+    --from-literal=NGC_API_KEY=$NGC_CLI_API_KEY \
+    --type=kubernetes.io/dockerconfigjson
+
+# Create PVC from storage class to request storage
+cat <<EOF | kubectl apply -f
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: my-pvc
+spec:
+  accessModes:
+    - ReadWriteMany
+  resources:
+    requests:
+      storage: 10Gi
+  storageClassName: $STORAGE_CLASS  # Replace this with the name of your storage class
+EOF
+
+# Apply configuration
+cat <<EOF | kubectl apply -f
+apiVersion: v1
+kind: Pod
+metadata:
+  name: llama-3-1-8b-instruct
+spec:
+  containers:
+  - name: llama-container
+    image: nvcr.io/nim/meta/llama-3.1-8b-instruct@sha256:e990eb6915c0917509318942bb5440291b0a2d498bdd6f2ebc1b8dfc4580f2d4
+    volumeMounts:
+    - mountPath: /mnt/models
+      name: my-storage
+    resources:
+      requests:
+        memory: "16Gi"
+        nvidia.com/gpu: 1
+      limits:
+        memory: "16Gi"
+        nvidia.com/gpu: 1
+    env:
+      valueFrom:
+        secretKeyRef:
+          name: ngc-secret
+          key: NGC_API_KEY
+    ports:
+    - containerPort: 8000
+    securityContext:
+      runAsUser: 1000  # Replace with your specific UID if different
+    # Configure shared memory size for better performance if necessary
+  imagePullSecrets:
+  - name: ngc-secret
+  volumes:
+    - name: my-storage
+      persistentVolumeClaim:
+        claimName: my-pvc
+EOF
---
+++ utils/test-ngc-commands.sh
@@ -0,0 +1,16 @@
+export NGC_CLI_API_KEY=nvapi-<>
+
+ngc config set
+ngc registry image info nim/nvidia/nv-embedqa-e5-v5
+ngc registry image info nvcr.io/nim/nvidia/nv-rerankqa-mistral-4b-v3
+
+ngc registry image info nvcr.io/nvidia/aiworkflows/rag-application-multiturn-chatbot
+ngc registry image info nvcr.io/nvidia/aiworkflows/rag-playground
+
+## validate that you can login via docker
+docker login nvcr.io -u $oauthtoken
+
+docker pull nvcr.io/nim/nvidia/nv-embedqa-e5-v5:1.0.1
+docker pull nvcr.io/nim/nvidia/nv-rerankqa-mistral-4b-v3:1.0.2
+docker pull nvcr.io/nvidia/aiworkflows/rag-playground:24.08
+docker pull nvcr.io/nvidia/aiworkflows/rag-application-multiturn-chatbot:24.08
