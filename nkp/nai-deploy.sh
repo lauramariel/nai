c77ea6837ae4f8fcf85faec9b2ba1d453aade434
Laura Jordana <lauram93@gmail.com>
2024-11-22 17:06:53 -0800

    update helm command for GA

--- nkp/nai-deploy.sh
+++ nkp/nai-deploy.sh
@@ -7,11 +7,13 @@ helm repo add ntnx-charts https://nutanix.github.io/helm-releases
 helm repo update ntnx-charts
 helm pull ntnx-charts/nai-core --version=$NAI_CORE_VERSION --untar=true
 
+export NAI_API_RWX_STORAGECLASS="nai-nfs-storage"
+
 #NAI-core
 helm upgrade --install nai-core ntnx-charts/nai-core --version=$NAI_CORE_VERSION -n nai-system --create-namespace --wait \
 --set imagePullSecret.credentials.username=$DOCKER_USERNAME \
 --set imagePullSecret.credentials.email=$DOCKER_EMAIL \
 --set imagePullSecret.credentials.password=$DOCKER_PASSWORD \
---set naiApi.naiApiImage.tag=v1.0.0-rc2 \
+--set naiApi.storageClassName=$NAI_API_RWX_STORAGECLASS \
 --insecure-skip-tls-verify \
 -f ./nai-core/$ENVIRONMENT-values.yaml
