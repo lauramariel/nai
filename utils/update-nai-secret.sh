090740b8ac98c37a7ce826b621cc4b28b87c8b80
Laura Jordana <lauram93@gmail.com>
2025-04-08 10:06:33 -0700

    script for updating NAI secret

--- utils/update-nai-secret.sh
+++ utils/update-nai-secret.sh
@@ -1,8 +1,9 @@
 # if you need to update nai-iep-secret for puling new images from DockerHub (e.g. the nai-model-processor image that's used the first time you download a model)
+# or if pods are restarted and need to pull the images again
 
 #!/bin/bash
 
-NAMESPACE="nai-admin"
+NAMESPACES=("nai-admin" "nai-system")
 SECRET_NAME="nai-iep-secret"
 REGISTRY_URL="https://index.docker.io/v1/"
 NEW_USERNAME=""
@@ -12,17 +13,22 @@ EMAIL=""
 # Generate a new .dockerconfigjson payload
 DOCKER_CONFIG_JSON=$(echo -n "{\"auths\":{\"$REGISTRY_URL\":{\"username\":\"$NEW_USERNAME\",\"password\":\"$NEW_PASSWORD\",\"email\":\"$EMAIL\"}}}" | base64 -w0)
 
-# Check if the secret exists
-kubectl get secret $SECRET_NAME -n $NAMESPACE > /dev/null 2>&1
+update_secret() {
+    local NAMESPACE=$1
+    # Check if the secret exists
+    kubectl get secret $SECRET_NAME -n $NAMESPACE > /dev/null 2>&1
 
-if [ $? -eq 0 ]; then
-    echo "Updating existing secret: $SECRET_NAME"
-    kubectl patch secret $SECRET_NAME -n $NAMESPACE --type='json' \
-      -p="[{'op':'replace','path':'/data/.dockerconfigjson','value':'$DOCKER_CONFIG_JSON'}]"
-else
-    echo "Secret name $SECRET_NAME not found."
-    exit 1
-fi
+    if [ $? -eq 0 ]; then
+        echo "Updating existing secret: $SECRET_NAME"
+        kubectl patch secret $SECRET_NAME -n $NAMESPACE --type='json' \
+        -p="[{'op':'replace','path':'/data/.dockerconfigjson','value':'$DOCKER_CONFIG_JSON'}]"
+    else
+        echo "Secret name $SECRET_NAME not found."
+        exit 1
+    fi
+}
 
-# Confirm the update
-kubectl get secret $SECRET_NAME -n $NAMESPACE -o yaml
+for NAMESPACE in "${NAMESPACES[@]}"; do
+    echo "Updating secret in namespace: $NAMESPACE"
+    update_secret $NAMESPACE
+done
