47221743e82a928e46d5646ad4bfcaae68cc428c
Laura Jordana <lauram93@gmail.com>
2025-08-26 15:35:40 -0700

    fix install cert script

--- nkp/nai-2.4/nai-install-cert.sh
+++ nkp/nai-2.4/nai-install-cert.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-## Using your own TLS cert that was generated for your FQDN
-bring_cert() {
+# Can also be used to update certificate
 read -p "Provide full path to certificate file: " CERT_PATH
 read -p "Provide full path to private key file: " KEY_PATH
+
+SECRET_NAME="nai-ingress-certificate"
 
 if [[ ! -e $CERT_PATH || ! -e $KEY_PATH ]]; then
     echo "ERROR: One or both files do not exist." >&2
     exit 1
 fi
 
-echo "Creating secret nai-ingress-certificate for certificate"
+echo "Creating secret $SECRET_NAME for certificate"
 
 # Create secret for certificates
-kubectl create secret tls nai-ingress-certificate -n nai-system --cert=$CERT_PATH --key=$KEY_PATH
+kubectl create secret tls $SECRET_NAME -n nai-system --cert=$CERT_PATH --key=$KEY_PATH --dry-run=client -o yaml | kubectl apply -f -
 
 # Patch gateway to use certificate
 kubectl patch gateway nai-ingress-gateway -n nai-system --type='json' --patch-file=/dev/stdin <<EOF
