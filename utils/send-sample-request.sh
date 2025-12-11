64f89b3ebdac6588de7ca648cf1303cd796d52cb
Laura Jordana <lauram93@gmail.com>
2025-12-10 17:45:04 -0800

    update payload for NAI 2.5

--- utils/send-sample-request.sh
+++ utils/send-sample-request.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -ex
 
-# export NAI_URL="10.28.131.191:30746"
-# export API_KEY="3d34d8fa-64c2-4b76-9f46-018ba4eae59a"
-# export NAI_EP="hiren-amx"
+export NAI_URL=""
+export API_KEY=""
+export NAI_EP=""
+export PROMPT="Help me plan a trip to Paris."
 
-export NAI_URL="nai.tmelab.net"
-export API_KEY="8cfbd98a-be81-44c8-9750-ca5c4844101c"
-export NAI_EP="mistral"
+# time curl -k -X 'POST' "https://$NAI_URL/api/v1/completions" -w '\nResponse Code: %{http_code}\n' -H 'accept: application/json' -H "Authorization: Bearer $API_KEY" -H 'Content-Type: application/json' -d "{
+# \"max_tokens\": 4096,
+# \"model\": \"$NAI_EP\",
+# \"prompt\": \"Help me plan a trip to Paris.\"
+# }"
 
-# export NAI_URL="ai.nutanix.com"
-# export API_KEY="4e2493cc-5251-4078-96e8-12d126eb62da"
-# export NAI_EP="llama-1b-ep"
-
-time curl -k -X 'POST' "https://$NAI_URL/api/v1/completions" -H 'accept: application/json' -H "Authorization: Bearer $API_KEY" -H 'Content-Type: application/json' -d "{
-\"max_tokens\": 200,
-\"model\": \"$NAI_EP\",
-\"prompt\": \"Help me plan a trip to Paris.\"
+# NAI 2.5
+time curl -k -X 'POST' "https://$NAI_URL/enterpriseai/v1/chat/completions" -w '\nResponse Code: %{http_code}\n' -H 'accept: application/json' -H "Authorization: Bearer $API_KEY" -H 'Content-Type: application/json' -d "{
+      \"model\": \"$NAI_EP\",
+      \"messages\": [
+        {
+          \"role\": \"user\",
+          \"content\": \"$PROMPT\"
+        }
+      ],
+      \"max_tokens\": 4096,
+      \"stream\": false
 }"
